35d0b8b17fa83f32468855b991ed3be96f124ba2
fr0mhell <t1m0feev.grigorij@gmail.com>
2022-05-27 01:29:03 +0300

    Puzzle L. Optimal iterative algorithm

--- puzzles/l.py
+++ puzzles/l.py
@@ -1,30 +1,14 @@
-from functools import lru_cache
-import sys
+def fibonacci(n, d):
+    a, b = 1, 1
 
-
-class RecursionDepthContext:
-
-    def __init__(self, new_limit):
-        self._system_limit = sys.getrecursionlimit()
-        self._new_limit = new_limit
-
-    def __enter__(self):
-        if self._new_limit > self._system_limit:
-            sys.setrecursionlimit(self._new_limit)
-
-    def __exit__(self, exc_type, exc_val, exc_tb):
-        sys.setrecursionlimit(self._system_limit)
-
-
-@lru_cache(maxsize=3)
-def fibonacci(n):
-    if n in (0, 1):
-        return 1
-    return fibonacci(n-1) + fibonacci(n-2)
+    if n >= 2:
+        for i in range(n-1):
+            a, b = b, (a + b) % d
+    return b
 
 
 if __name__ == '__main__':
     n, k = (int(i) for i in input().split())
-    with RecursionDepthContext(3 * n):
-        result = fibonacci(n)
-    print(result % 10 ** k)
+    d = (10 ** k)
+
+    print(fibonacci(n, d))
