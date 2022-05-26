43e5767a01c625e6034bb2d1681e032e881edfab
fr0mhell <t1m0feev.grigorij@gmail.com>
2022-05-27 00:12:30 +0300

    Puzzle L. Add simple recursive implementation

---
+++ puzzles/l.py
@@ -0,0 +1,10 @@
+def fibonacci(n):
+    if n in (0, 1):
+        return 1
+    return fibonacci(n-1) + fibonacci(n-2)
+
+
+if __name__ == '__main__':
+    n, k = (int(i) for i in input().split())
+    result = fibonacci(n)
+    print(result % 10 ** k)
---
+++ tests/test_l.py
@@ -0,0 +1,15 @@
+from unittest import TestCase
+
+from puzzles.l import fibonacci
+
+
+class PuzzleLTestCase(TestCase):
+
+    @classmethod
+    def setUpClass(cls) -> None:
+        cls.expected = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377, 610]
+
+    def test_fibonacci(self):
+        for n, expected in enumerate(self.expected):
+            with self.subTest(n=n, expected=expected):
+                self.assertEqual(fibonacci(n), expected)
