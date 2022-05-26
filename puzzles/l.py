b3b9fd7a8a7c2e314712ccc25c5ae8d0b4d4315c
fr0mhell <t1m0feev.grigorij@gmail.com>
2022-05-27 01:06:01 +0300

    Puzzle L. Change recursion depth

--- puzzles/l.py
+++ puzzles/l.py
@@ -1,4 +1,19 @@
 from functools import lru_cache
+import sys
+
+
+class RecursionDepthContext:
+
+    def __init__(self, new_limit):
+        self._system_limit = sys.getrecursionlimit()
+        self._new_limit = new_limit
+
+    def __enter__(self):
+        if self._new_limit > self._system_limit:
+            sys.setrecursionlimit(self._new_limit)
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        sys.setrecursionlimit(self._system_limit)
 
 
 @lru_cache(maxsize=3)
@@ -10,5 +25,6 @@ def fibonacci(n):
 
 if __name__ == '__main__':
     n, k = (int(i) for i in input().split())
-    result = fibonacci(n)
+    with RecursionDepthContext(3 * n):
+        result = fibonacci(n)
     print(result % 10 ** k)
